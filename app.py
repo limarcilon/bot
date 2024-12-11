0cb5b6603b69dde5b3b739efd1525b5ca0f90100
Liliana Marcilon <liliana.marcilon@ebanx.com>
2024-12-11 16:12:16 -0300

    testando emoji

--- app.py
+++ app.py
@@ -7,39 +7,33 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-# Variáveis de ambiente para autenticação
-SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')  # Bot OAuth Token do Slack
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
 slack_client = slack_sdk.WebClient(token=SLACK_BOT_TOKEN)
 
 @app.route('/slack/events', methods=['POST'])
 def slack_events():
     data = request.json
 
-    # Verificação de URL durante a configuração do evento
     if "challenge" in data:
         return jsonify({"challenge": data["challenge"]})
 
-    # Processar eventos de reactions
     if "event" in data:
         event = data["event"]
 
-        # Verifica se é um evento de reaction
         if event.get('type') == 'reaction_added':
             channel = event.get('item', {}).get('channel')
             timestamp = event.get('item', {}).get('ts')
             reaction = event.get('reaction')
 
             try:
-                # Adiciona uma reaction na mensagem
                 slack_client.reactions_add(
                     channel=channel,
-                    name="thumbsup",  # Reação que você deseja adicionar
+                    name="thumbsup",
                     timestamp=timestamp
                 )
             except SlackApiError as e:
-                print(f"Error2 adding reaction: {e.response['error']}")
+                print(f"Error adding reaction: {e.response['error']}")
 
-        # Outros eventos podem ser processados da mesma maneira
     return jsonify({"status": "ok"})
 
 
